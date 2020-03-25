b8eb54d8d4d51d5492fd9b451527763e643fcb51
cklinton <60175636+cklinton@users.noreply.github.com>
2020-03-25 13:24:26 +0100

    Added "dummy" unit test

--- PCAxis.Menu.OracleDatamodelMenu.UnitTest/UnitTest1.cs
+++ PCAxis.Menu.OracleDatamodelMenu.UnitTest/UnitTest1.cs
@@ -8,6 +8,16 @@ namespace PCAxis.Menu.OracleDatamodelMenu.UnitTest
 		[TestMethod]
 		public void TestMethod1()
 		{
+			//Arrange
+			var a = 0;
+			var b = 0;
+
+			//Act
+
+			//Assert
+			//Check their equality.
+			Assert.AreEqual(a, b);
+
 		}
 	}
 }
